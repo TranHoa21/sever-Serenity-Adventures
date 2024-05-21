0248d739dba7bc9664d7bd09fd327bca6a34e5cb
Tran Thanh Hoa <139173700+TranHoa21@users.noreply.github.com>
2024-05-21 18:33:43 +0700

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
 
-    origin: ['https://serenity-adventures-demo.vercel.app', 'https://admin-serenity-adventures.vercel.app', 'http://localhost:3000', 'http://localhost:3002'],
+    origin: ['https://serenity-adventures-demo.vercel.app', 'https://serenity-adventures-admin.vercel.app', 'http://localhost:3000', 'http://localhost:3002'],
     credentials: true
 }));
 
@@ -26,7 +26,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
 
-        origin: ["https://serenity-adventures-demo.vercel.app/", "https://admin-serenity-adventures.vercel.app", 'http://localhost:3000', 'http://localhost:3002'],
+        origin: ["https://serenity-adventures-demo.vercel.app/", "https://serenity-adventures-admin.vercel.app", 'http://localhost:3000', 'http://localhost:3002'],
         methods: ["GET", "POST"],
         credentials: true
     },
